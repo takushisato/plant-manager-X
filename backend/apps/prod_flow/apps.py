76c1e8b63be17aac6556b1d78cedb70a0bf5a775
DESKTOP-PEUAMU0\takuc <takuchanpower@gmail.com>
2025-03-20 14:20:45 +0900

    backendの新規構築

---
+++ backend/apps/prod_flow/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProdFlowConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.prod_flow'
---
+++ backend/apps/staff_hub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StaffHubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.staff_hub'
