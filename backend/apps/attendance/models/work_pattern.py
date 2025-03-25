42e2bf986b17b7542664b3a04fcab8077417d7a6
takushisato <takuchanpower@gmail.com>
2025-03-25 16:03:26 +0900

    勤務形態モデルの追加

---
+++ backend/apps/attendance/models/work_pattern.py
@@ -0,0 +1,19 @@
+from django.db import models
+from apps.utility.models import BaseModel
+
+
+class WorkPattern(BaseModel):
+    """
+    勤務形態モデル
+    """
+    work_pattern_name = models.CharField("勤務形態名", max_length=255)
+    start_time = models.TimeField("勤務開始時間")
+    end_time = models.TimeField("勤務終了時間")
+    break_total_minute = models.IntegerField("1日の休憩時間", default=0)
+    note = models.CharField("備考", max_length=1000, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '勤務形態'
+        verbose_name_plural = '勤務形態'
+        db_table = 'work_patterns'
+
