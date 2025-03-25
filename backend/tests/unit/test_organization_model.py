1d040cd7a3794bb61d1f8ad4f85f43b79c8fe9b8
takushisato <takuchanpower@gmail.com>
2025-03-25 14:17:15 +0900

    組織のUnitテスト追加

---
+++ backend/tests/unit/test_organization_model.py
@@ -0,0 +1,36 @@
+import pytest
+from django.db.utils import IntegrityError
+from apps.staff_hub.models import Organization
+from tests.factory.organization_factory import OrganizationFactory
+
+
+@pytest.mark.django_db
+def test_create_organization():
+    """
+    Organization インスタンスを正常に作成できる
+    """
+    org = Organization.objects.create(
+        organization_name="Test Company",
+        description="これはテスト用の組織です。"
+    )
+    assert org.organization_name == "Test Company"
+    assert org.description == "これはテスト用の組織です。"
+
+
+@pytest.mark.django_db
+def test_organization_str():
+    """
+    __str__ メソッドが organization_name を返すこと
+    """
+    org = OrganizationFactory(organization_name="My Org")
+    assert str(org) == "My Org"
+
+
+@pytest.mark.django_db
+def test_organization_name_unique():
+    """
+    organization_name がユニークであることを確認する
+    """
+    Organization.objects.create(organization_name="UniqueOrg")
+    with pytest.raises(IntegrityError):
+        Organization.objects.create(organization_name="UniqueOrg")
