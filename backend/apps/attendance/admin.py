fdcfcbf55afb8d52e4b9618e44478e4e6b912dab
takushisato <takuchanpower@gmail.com>
2025-03-25 16:56:31 +0900

    admin更新

--- backend/apps/attendance/admin.py
+++ backend/apps/attendance/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models.paid_leave import PaidLeave
+from .models.break_setting import BreakSetting
+from .models.work_pattern import WorkPattern
 
 
 admin.site.register(PaidLeave)
+admin.site.register(BreakSetting)
+admin.site.register(WorkPattern)
