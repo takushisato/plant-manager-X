dda1b769ec0cd3307b7dac0d65812a0cac4e0968
takushisato <takuchanpower@gmail.com>
2025-03-25 16:28:44 +0900

    休憩時間モデルを追加

--- backend/apps/attendance/models/__init__.py
+++ backend/apps/attendance/models/__init__.py
@@ -1,1 +1,2 @@
 from .work_pattern import models
+from .break_setting import models
---
+++ backend/apps/attendance/models/break_setting.py
@@ -0,0 +1,23 @@
+from django.db import models
+from apps.utility.models import BaseModel
+from apps.attendance.models.work_pattern import WorkPattern
+
+
+class BreakSetting(BaseModel):
+    """
+    休憩時間設定モデル
+    """
+    work_pattern_id = models.ForeignKey(WorkPattern, on_delete=models.CASCADE, verbose_name="勤務形態")
+    start_time = models.TimeField("休憩開始時間")
+    end_time = models.TimeField("休憩終了時間")
+    note = models.CharField("備考", max_length=1000, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '休憩時間設定'
+        verbose_name_plural = '休憩時間設定'
+        db_table = 'break_settings'
+
+    def __str__(self):
+        return f"{self.work_pattern_id.work_pattern_name} - {self.start_time} - {self.end_time}"
+
+
