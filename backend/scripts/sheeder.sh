2cff8eb63d91b78c4fea83c30616cd13f39baef0
takushisato <takuchanpower@gmail.com>
2025-03-26 14:59:01 +0900

    勤務関係のsheederを追加

--- backend/scripts/sheeder.sh
+++ backend/scripts/sheeder.sh
@@ -11,3 +11,15 @@ python manage.py loaddata permissions.json
 
 # 資材データの読み込み
 python manage.py loaddata materials.json
+
+# 勤務形態データの読み込み
+python manage.py loaddata work_patterns.json
+
+# 休憩時間設定データの読み込み
+python manage.py loaddata break_settings.json
+
+# 有給休暇データの読み込み
+python manage.py loaddata paid_leaves.json
+
+# 勤怠データの読み込み
+python manage.py loaddata attendance_records.json
