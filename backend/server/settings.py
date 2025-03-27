6641a506c9f614eb564b402c0797906457c3cbfa
takushisato <takuchanpower@gmail.com>
2025-03-27 18:54:26 +0900

    設定更新

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -153,7 +153,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 DJOSER = {
