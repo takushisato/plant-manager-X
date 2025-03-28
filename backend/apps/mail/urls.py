3d2a3e698c87f289b85ffebd7366794ff6cb1216
takushisato <takuchanpower@gmail.com>
2025-03-28 18:46:24 +0900

    宛先の一括取得APIを追加

--- backend/apps/mail/urls.py
+++ backend/apps/mail/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from apps.mail.views.mail_group_create_view import MailGroupCreateView
 from apps.mail.views.mail_group_detail_create_view import MailGroupDetailBulkCreateView
+from apps.mail.views.mail_group_list_view import MailGroupListView
 
 
 urlpatterns = [
+    path("groups/", MailGroupListView.as_view(), name="mail-group-list"),
     path("groups/create/", MailGroupCreateView.as_view(), name="mailgroup-create"),
     path("groups/details/bulk-create/", MailGroupDetailBulkCreateView.as_view(), name="mail-group-detail-bulk-create"),
 ]
