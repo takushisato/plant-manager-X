7510cd7f206e8a91c0d7c6c5d62bd744b54619c7
takushisato <takuchanpower@gmail.com>
2025-03-28 17:49:34 +0900

    メールグループ詳細作成のURLを追加

--- backend/apps/mail/urls.py
+++ backend/apps/mail/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from apps.mail.views.mail_group_create_view import MailGroupCreateView
+from apps.mail.views.mail_group_detail_create_view import MailGroupDetailBulkCreateView
 
 
 urlpatterns = [
     path("groups/create/", MailGroupCreateView.as_view(), name="mailgroup-create"),
+    path("groups/details/bulk-create/", MailGroupDetailBulkCreateView.as_view(), name="mail-group-detail-bulk-create"),
 ]
