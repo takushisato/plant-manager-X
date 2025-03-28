0fbae9ee340d9b02bbbdfe8a317156a7ab96988f
takushisato <takuchanpower@gmail.com>
2025-03-28 14:23:08 +0900

    資材受け入れAPIの追加

--- backend/apps/material/urls.py
+++ backend/apps/material/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UseStockView, MaterialListView
+from .views import UseStockView, MaterialListView, ReceiveStockView
 
 urlpatterns = [
     path("", MaterialListView.as_view(), name="material-list"),
     path("<int:pk>/use_stock/", UseStockView.as_view(), name="use-stock"),
+    path("<int:pk>/receive_stock/", ReceiveStockView.as_view(), name="receive-stock"),
 ]
