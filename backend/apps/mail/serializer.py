2758db4757e597a8ed333507007099a3fe1735c8
takushisato <takuchanpower@gmail.com>
2025-03-28 18:46:05 +0900

    GETメソッドを追加

--- backend/apps/mail/serializer.py
+++ backend/apps/mail/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from apps.mail.models.mail_group import MailGroup
+from apps.mail.models.mail_group_detail import MailGroupDetail
 from apps.staff_hub.models import User
 
 
@@ -15,3 +16,22 @@ class MailGroupDetailBulkCreateSerializer(serializers.Serializer):
         many=True,
         queryset=User.objects.all()
     )
+
+class MailGroupDetailSerializer(serializers.ModelSerializer):
+    recipient_user_name = serializers.CharField(source="recipient_user.name", read_only=True)
+
+    class Meta:
+        model = MailGroupDetail
+        fields = ["id", "recipient_user", "recipient_user_name"]
+
+
+class MailGroupWithDetailsSerializer(serializers.ModelSerializer):
+    details = MailGroupDetailSerializer(
+        source="mailgroupdetail_set",
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = MailGroup
+        fields = ["id", "group_title", "note", "details"]
