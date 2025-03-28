e358327406fc212394bc768bc15ea77c5ce765cf
takushisato <takuchanpower@gmail.com>
2025-03-28 12:33:04 +0900

    取得のAPI追加

--- backend/apps/material/urls.py
+++ backend/apps/material/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UseStockView
+from .views import UseStockView, MaterialListView
 
 urlpatterns = [
+    path("", MaterialListView.as_view(), name="material-list"),
     path("<int:pk>/use_stock/", UseStockView.as_view(), name="use-stock"),
 ]
