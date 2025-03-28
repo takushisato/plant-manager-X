a1f4422b647e90c022793748f26742dfb0063d9e
takushisato <takuchanpower@gmail.com>
2025-03-28 17:49:16 +0900

    メールグループ詳細の追加

--- backend/apps/mail/serializer.py
+++ backend/apps/mail/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from apps.mail.models.mail_group import MailGroup
+from apps.staff_hub.models import User
 
 
 class MailGroupCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = MailGroup
         fields = ['group_title', 'note']
+
+
+class MailGroupDetailBulkCreateSerializer(serializers.Serializer):
+    mail_group_detail = serializers.PrimaryKeyRelatedField(queryset=MailGroup.objects.all())
+    recipient_users = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
+    )
