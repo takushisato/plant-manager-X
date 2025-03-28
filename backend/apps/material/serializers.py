ef656a0da302149f625a9035245c79294d6f8bcf
takushisato <takuchanpower@gmail.com>
2025-03-28 15:26:59 +0900

    シリアライザーのバリデーションをviewに移動

--- backend/apps/material/serializers.py
+++ backend/apps/material/serializers.py
@@ -5,12 +5,6 @@ from apps.material.models.material import Material
 
 class UseStockSerializer(serializers.Serializer):
     used_qty = serializers.IntegerField(min_value=1)
-
-    def validate_used_qty(self, value):
-        material = self.context["material"]
-        if value > material.stock_qty:
-            raise serializers.ValidationError("在庫が不足しています。")
-        return value
     
 
 class MaterialSerializer(serializers.ModelSerializer):
@@ -20,6 +14,7 @@ class MaterialSerializer(serializers.ModelSerializer):
             "id", "organization", "material_name", "material_price",
             "stock_qty", "order_suggestion_qty",
         ]
+
 
 class ReceiveStockSerializer(serializers.Serializer):
     added_qty = serializers.IntegerField()
