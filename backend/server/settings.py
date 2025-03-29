62b452fe03a572ad82e4d4911eb4be68dbc44ef1
takushisato <takuchanpower@gmail.com>
2025-03-29 15:26:24 +0900

    設定追加

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 import environ
 
 env = environ.Env()
@@ -215,8 +215,8 @@ AUTH_USER_MODEL = 'staff_hub.User'
 # メール送信設定
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
