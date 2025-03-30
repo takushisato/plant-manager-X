00bec54e292e2c000655d49856d9d23f7f25e3e3
takushisato <takuchanpower@gmail.com>
2025-03-30 14:10:02 +0900

    テストも修正

--- backend/tests/factory/permission_factory.py
+++ backend/tests/factory/permission_factory.py
@@ -14,10 +14,14 @@ class PermissionFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(UserFactory)
     staff_hub_access = False
     material_access = False
-    attendance_access = False
-    prod_flow_access = False
-    trade_flow_access = False
-    bug_note_access = False
+    can_manage_own_attendance = False
+    can_manage_all_attendance = False
+    can_view_production_plan = False
+    can_edit_production_plan = False
+    can_view_order = False
+    can_edit_order = False
+    can_view_defect = False
+    can_edit_defect = False
     mail_access = False
     master_data_access = False
 
@@ -25,10 +29,14 @@ class PermissionFactory(factory.django.DjangoModelFactory):
         full_access = factory.Trait(
             staff_hub_access=True,
             material_access=True,
-            attendance_access=True,
-            prod_flow_access=True,
-            trade_flow_access=True,
-            bug_note_access=True,
+            can_manage_own_attendance=True,
+            can_manage_all_attendance=True,
+            can_view_production_plan=True,
+            can_edit_production_plan=True,
+            can_view_order=True,
+            can_edit_order=True,
+            can_view_defect=True,
+            can_edit_defect=True,
             mail_access=True,
             master_data_access=True,
         )
