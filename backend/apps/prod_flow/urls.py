923072c5ed5f6b01d6f7b1449072cb289ec380b2
takushisato <takuchanpower@gmail.com>
2025-03-31 14:40:55 +0900

    updateを追加

--- backend/apps/prod_flow/urls.py
+++ backend/apps/prod_flow/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from apps.prod_flow.views.production_plan_with_details_create_view import ProductionPlanWithDetailsCreateView
 from apps.prod_flow.views.production_plan_list_view import ProductionPlanListView
+from apps.prod_flow.views.production_plan_with_details_update_view import ProductionPlanWithDetailsUpdateView
 
 
 urlpatterns = [
     path("plans/", ProductionPlanListView.as_view(), name="plan-list"),
     path("plan_with_details/create/", ProductionPlanWithDetailsCreateView.as_view(), name="plan_with_details_create"),
+    path("plans/<int:pk>/update/", ProductionPlanWithDetailsUpdateView.as_view(), name="production-plan-update"),
 ]
