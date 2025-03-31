e3d87680fb8a9664c83aaafc9f1d1fae6c7af604
takushisato <takuchanpower@gmail.com>
2025-03-31 16:29:52 +0900

    編集権限追加

--- backend/apps/prod_flow/common.py
+++ backend/apps/prod_flow/common.py
@@ -7,3 +7,11 @@ def check_prod_flow_access_permission(request):
     """
     if not (request.permission.can_view_production_plan or request.permission.can_edit_production_plan or request.permission.master_data_access):
         raise PermissionDenied("認証は確認しましたが権限がありません。")
+
+def check_prod_flow_edit_permission(request):
+    """
+    生産計画編集権限チェック
+    """
+    if not (request.permission.can_edit_production_plan or request.permission.master_data_access):
+        raise PermissionDenied("認証は確認しましたが権限がありません。")
+
