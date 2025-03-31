bf7e35cf33c224adc2f5ffee5f90d692fd3ba30d
takushisato <takuchanpower@gmail.com>
2025-03-31 13:41:34 +0900

    生産計画一括取得APIを作成

--- backend/apps/prod_flow/urls.py
+++ backend/apps/prod_flow/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from apps.prod_flow.views.production_plan_with_details_create_view import ProductionPlanWithDetailsCreateView
+from apps.prod_flow.views.production_plan_list_view import ProductionPlanListView
+
 
 urlpatterns = [
+    path("plans/", ProductionPlanListView.as_view(), name="plan-list"),
     path("plan_with_details/create/", ProductionPlanWithDetailsCreateView.as_view(), name="plan_with_details_create"),
 ]
