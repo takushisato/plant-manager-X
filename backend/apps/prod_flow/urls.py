c684f5834794d3378d07d5c68b8c09424aa3834a
takushisato <takuchanpower@gmail.com>
2025-03-31 15:51:55 +0900

    論理削除APIを追加

--- backend/apps/prod_flow/urls.py
+++ backend/apps/prod_flow/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from apps.prod_flow.views.production_plan_with_details_create_view import ProductionPlanWithDetailsCreateView
-from apps.prod_flow.views.production_plan_list_view import ProductionPlanListView
-from apps.prod_flow.views.production_plan_with_details_update_view import ProductionPlanWithDetailsUpdateView
+from apps.prod_flow.views import ProductionPlanListView, ProductionPlanWithDetailsCreateView, ProductionPlanWithDetailsUpdateView, ProductionPlanDeleteView
 
 
 urlpatterns = [
     path("plans/", ProductionPlanListView.as_view(), name="plan-list"),
     path("plan_with_details/create/", ProductionPlanWithDetailsCreateView.as_view(), name="plan_with_details_create"),
     path("plans/<int:pk>/update/", ProductionPlanWithDetailsUpdateView.as_view(), name="production-plan-update"),
+    path('plans/<int:pk>/delete/', ProductionPlanDeleteView.as_view(), name='production-plan-delete'),
 ]
