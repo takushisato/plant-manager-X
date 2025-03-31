7ddd9dd4cadd9cefb5cbc10b102891064fc43669
takushisato <takuchanpower@gmail.com>
2025-03-31 18:26:12 +0900

    一括取得のAPI追加

--- backend/apps/trade_flow/urls.py
+++ backend/apps/trade_flow/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderCreateView
+from .views import OrderCreateView, OrderListView
 
 urlpatterns = [
+    path("orders/list/", OrderListView.as_view(), name="order-list"),
     path("orders/create/", OrderCreateView.as_view(), name="order-create"),
 ]
