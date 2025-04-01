d57e1fabcfadb256a6f112ad4d7104b32f2d792b
takushisato <takuchanpower@gmail.com>
2025-04-01 15:15:00 +0900

    不具合情報の詳細取得API作成

--- backend/apps/bug_note/common.py
+++ backend/apps/bug_note/common.py
@@ -5,7 +5,7 @@ def check_bug_note_view_permission(request):
     """
     不具合情報：閲覧権限チェック
     """
-    if not (request.permission.can_view_defect or request.permission.master_data_access):
+    if not (request.permission.can_view_defect or request.permission.can_edit_defect or request.permission.master_data_access):
         raise PermissionDenied("認証は確認しましたが権限がありません。")
 
 def check_bug_note_edit_permission(request):
