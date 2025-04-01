50d6b22bf802cef5f7f688dc25b9bcf0e1d90ab5
takushisato <takuchanpower@gmail.com>
2025-04-01 10:41:29 +0900

    更新のAPIを追加

--- backend/apps/trade_flow/urls.py
+++ backend/apps/trade_flow/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderCreateView, OrderListView
+from .views import OrderCreateView, OrderListView, OrderUpdateView
 
 urlpatterns = [
     path("orders/list/", OrderListView.as_view(), name="order-list"),
     path("orders/create/", OrderCreateView.as_view(), name="order-create"),
+    path("orders/<int:pk>/update/", OrderUpdateView.as_view(), name="order-update"),
 ]
