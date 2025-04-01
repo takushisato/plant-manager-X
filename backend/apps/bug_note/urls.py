a2d3c289dc04fbd3795a1da4b20ca2c600778b71
takushisato <takuchanpower@gmail.com>
2025-04-01 16:57:18 +0900

    不具合情報の論理削除APIを追加

--- backend/apps/bug_note/urls.py
+++ backend/apps/bug_note/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.bug_note.views import DefectCreateView, DefectListView, DefectDetailView, DefectUpdateView
+from apps.bug_note.views import DefectCreateView, DefectListView, DefectDetailView, DefectUpdateView, DefectDeleteView
 
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path("defects/list/", DefectListView.as_view(), name="defect-list"),
     path("defects/<int:pk>/", DefectDetailView.as_view(), name="defect-detail"),
     path("defects/<int:pk>/update/", DefectUpdateView.as_view(), name="defect-update"),
-
+    path('defects/<int:pk>/delete/', DefectDeleteView.as_view(), name='defect-delete'),
 ]
