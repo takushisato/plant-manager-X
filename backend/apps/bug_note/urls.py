0e25f4753e2c4178b902a680918426e91debbcc5
takushisato <takuchanpower@gmail.com>
2025-04-01 15:15:15 +0900

    不具合情報の詳細取得API作成

--- backend/apps/bug_note/urls.py
+++ backend/apps/bug_note/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.bug_note.views import DefectCreateView, DefectListView
+from apps.bug_note.views import DefectCreateView, DefectListView, DefectDetailView
 
 
 urlpatterns = [
     path("defects/create/", DefectCreateView.as_view(), name="defect-create"),
     path("defects/list/", DefectListView.as_view(), name="defect-list"),
+    path("defects/<int:pk>/", DefectDetailView.as_view(), name="defect-detail"),
 ]
