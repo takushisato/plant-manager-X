de75c8b82d7270bd37f1d926270d8842f35558c2
takushisato <takuchanpower@gmail.com>
2025-04-01 16:30:06 +0900

    不具合情報更新APIを追加

--- backend/apps/bug_note/urls.py
+++ backend/apps/bug_note/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from apps.bug_note.views import DefectCreateView, DefectListView, DefectDetailView
+from apps.bug_note.views import DefectCreateView, DefectListView, DefectDetailView, DefectUpdateView
 
 
 urlpatterns = [
     path("defects/create/", DefectCreateView.as_view(), name="defect-create"),
     path("defects/list/", DefectListView.as_view(), name="defect-list"),
     path("defects/<int:pk>/", DefectDetailView.as_view(), name="defect-detail"),
+    path("defects/<int:pk>/update/", DefectUpdateView.as_view(), name="defect-update"),
+
 ]
