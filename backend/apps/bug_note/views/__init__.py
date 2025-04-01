efdcaad8c87bdb47ae47e56dee7b211a80035af8
takushisato <takuchanpower@gmail.com>
2025-04-01 16:30:13 +0900

    不具合情報更新APIを追加

--- backend/apps/bug_note/views/__init__.py
+++ backend/apps/bug_note/views/__init__.py
@@ -1,3 +1,4 @@
 from .defect_create_view import DefectCreateView
 from .defect_list_view import DefectListView
 from .defect_detail_view import DefectDetailView
+from .defect_update_view import DefectUpdateView
