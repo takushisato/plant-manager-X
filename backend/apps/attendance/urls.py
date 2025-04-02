c28f38315b175c12ef90ad925f72a565275533ce
takushisato <takuchanpower@gmail.com>
2025-04-02 17:38:24 +0900

    勤怠の更新APIを追加

--- backend/apps/attendance/urls.py
+++ backend/apps/attendance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.attendance.views import RecordCreateView, MonthlyAttendanceRecordListView, MonthlyAttendanceRecordAllListView
+from apps.attendance.views import RecordCreateView, MonthlyAttendanceRecordListView, MonthlyAttendanceRecordAllListView, RecordUpdateView
 
 
 urlpatterns = [
-    path("record/create/", RecordCreateView.as_view(), name="attendance-record-create"),
     path("records/list/", MonthlyAttendanceRecordListView.as_view(), name="attendance-record-list"),
+    path("record/create/", RecordCreateView.as_view(), name="attendance-record-create"),
+    path("records/<int:pk>/update/", RecordUpdateView.as_view(), name="attendance-record-update"),
     path("records/all/list/", MonthlyAttendanceRecordAllListView.as_view(), name="attendance-record-all-list"),
 ]
