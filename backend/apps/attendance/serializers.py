d01d3ee9db92b8a871a0723b335619e974709aff
takushisato <takuchanpower@gmail.com>
2025-04-02 12:07:25 +0900

    勤怠入力のAPI作成

---
+++ backend/apps/attendance/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from apps.attendance.models.attendance_record import AttendanceRecord
+
+
+class AttendanceRecordCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AttendanceRecord
+        exclude = ['user']
