09d6f5207ea51795d08ea3047bcd34a28dc9e757
takushisato <takuchanpower@gmail.com>
2025-04-02 15:53:02 +0900

    自分の勤怠情報取得APIを追加

--- backend/apps/attendance/serializers.py
+++ backend/apps/attendance/serializers.py
@@ -6,3 +6,11 @@ class RecordCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Record
         exclude = ['user', 'work_minutes', 'break_minutes']
+
+class RecordListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = [
+            "id", "work_date", "clock_in_time", "clock_out_time",
+            "work_minutes", "break_minutes", "work_status", "note"
+        ]
