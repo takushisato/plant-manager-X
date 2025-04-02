3bcfd144b13cab5633e043f6ce9cb7cdde0ddceb
takushisato <takuchanpower@gmail.com>
2025-04-02 17:38:16 +0900

    勤怠の更新APIを追加

--- backend/apps/attendance/serializers.py
+++ backend/apps/attendance/serializers.py
@@ -14,3 +14,21 @@ class RecordListSerializer(serializers.ModelSerializer):
             "id", "work_date", "clock_in_time", "clock_out_time",
             "work_minutes", "break_minutes", "work_status", "note"
         ]
+
+
+class RecordUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = [
+            "work_pattern", "work_date", "clock_in_time", "clock_out_time",
+            "work_status", "note"
+        ]
+
+    def validate(self, attrs):
+        clock_in = attrs.get("clock_in_time", self.instance.clock_in_time)
+        clock_out = attrs.get("clock_out_time", self.instance.clock_out_time)
+
+        if clock_out <= clock_in:
+            raise serializers.ValidationError("退勤時間は出勤時間より後である必要があります。")
+
+        return attrs
