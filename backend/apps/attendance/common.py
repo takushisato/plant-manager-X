484938e767a8ff4fe968307b5db0ea08c48b428b
takushisato <takuchanpower@gmail.com>
2025-04-02 12:07:19 +0900

    勤怠入力のAPI作成

---
+++ backend/apps/attendance/common.py
@@ -0,0 +1,17 @@
+from rest_framework.exceptions import PermissionDenied
+
+
+def check_attendance_own_edit_permission(request):
+    """
+    自分の勤怠のみ編集できる権限
+    """
+    if not (request.permission.can_manage_own_attendance or request.permission.master_data_access):
+        raise PermissionDenied("認証は確認しましたが権限がありません。")
+
+def check_attendance_all_edit_permission(request):
+    """
+    全員の勤怠を編集できる権限
+    """
+    if not (request.permission.can_manage_all_attendance or request.permission.master_data_access):
+        raise PermissionDenied("認証は確認しましたが権限がありません。")
+
