3f7604bb87272d9c5f21214c01cdd2f0acd4e9b5
takushisato <takuchanpower@gmail.com>
2025-04-02 16:30:33 +0900

    全員の勤怠記録取得APIを追加

--- backend/apps/attendance/urls.py
+++ backend/apps/attendance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.attendance.views import RecordCreateView, MonthlyAttendanceRecordListView
+from apps.attendance.views import RecordCreateView, MonthlyAttendanceRecordListView, MonthlyAttendanceRecordAllListView
 
 
 urlpatterns = [
     path("record/create/", RecordCreateView.as_view(), name="attendance-record-create"),
     path("records/list/", MonthlyAttendanceRecordListView.as_view(), name="attendance-record-list"),
+    path("records/all/list/", MonthlyAttendanceRecordAllListView.as_view(), name="attendance-record-all-list"),
 ]
