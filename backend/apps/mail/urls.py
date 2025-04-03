1211ee368a0fae5e63cbfcee29825afc877aa60e
takushisato <takuchanpower@gmail.com>
2025-04-03 17:57:23 +0900

    メール関係のエンドポイント見直し

--- backend/apps/mail/urls.py
+++ backend/apps/mail/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from apps.mail.views.mail_group_detail_create_view import MailGroupDetailBulkCreateView
 from apps.mail.views.mail_group_view import MailGroupView
 from apps.mail.views.mail_group_detail_view import MailGroupDetailView
+from apps.mail.views.mail_group_bulk_create_view import MailGroupBulkCreateView
 from apps.mail.views.mail_send_view import MailSendView
 
 
 urlpatterns = [
     path("groups/", MailGroupView.as_view(), name="mail-group"),
     path("groups/<int:pk>/", MailGroupDetailView.as_view(), name="mail-group-detail"),
-    path("groups/details/bulk-create/", MailGroupDetailBulkCreateView.as_view(), name="mail-group-detail-bulk-create"),
-    path("groups/send/", MailSendView.as_view(), name="mail-send"),
+    path("groups/bulk-create/", MailGroupBulkCreateView.as_view(), name="mail-group-bulk-create"),
+    path("groups/send/", MailSendView.as_view(), name="mail-group-send"),
 ]
--- backend/apps/mail/views/__init__.py
+++ backend/apps/mail/views/__init__.py
@@ -1,3 +1,4 @@
 from .mail_group_view import MailGroupView
-from .mail_group_detail_create_view import MailGroupDetailBulkCreateView
+from .mail_group_bulk_create_view import MailGroupBulkCreateView
 from .mail_group_detail_view import MailGroupDetailView
+from .mail_send_view import MailSendView
