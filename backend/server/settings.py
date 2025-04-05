16e10d120ea709f272edbd21b362b06bda2b352a
takushisato <takuchanpower@gmail.com>
2025-04-05 13:07:55 +0900

    startを追加

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'NAME': env('DB_NAME', default='mydatabase'),
         'USER': env('DB_USER', default='user'),
         'PASSWORD': env('DB_PASSWORD', default='password'),
-        'HOST': env('DB_HOST', default='http://127.0.0.1'),
+        'HOST': env('DB_HOST', default='mysql'),
         'PORT': env.int('DB_PORT', default=3306),
     }
 }
