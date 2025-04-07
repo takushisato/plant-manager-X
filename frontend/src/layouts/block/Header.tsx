b814e13444c5da2e4468743bb1c25d31c4d52028
takushisato <takuchanpower@gmail.com>
2025-04-07 18:32:10 +0900

    ヘッダーのたたき台作成

--- frontend/src/layouts/block/Header.tsx
+++ frontend/src/layouts/block/Header.tsx
@@ -1,8 +1,30 @@
+import { Box, Flex, Heading, IconButton, Spacer, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react"
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <Box as="header" bg="teal.500" px={4} py={3} color="white">
+      <Flex align="center">
+        <Heading size="md">MySite</Heading>
+        <Spacer />
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<HamburgerIcon />}
+            variant="outline"
+            color="white"
+            borderColor="white"
+            _hover={{ bg: 'teal.600' }}
+          />
+          <MenuList>
+            <MenuItem as={Link} to="/">Home</MenuItem>
+            <MenuItem as={Link} to="/about">About</MenuItem>
+            <MenuItem as={Link} to="/contact">Contact</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Box>
   );
 };
 
