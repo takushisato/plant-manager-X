527964a4dc6f0fd7082f9a716bd85c161df1980c
takushisato <takuchanpower@gmail.com>
2025-04-07 16:38:50 +0900

    不要ファイル削除

--- frontend/src/layouts/index.ts
+++
@@ -1,5 +0,0 @@
-import AuthLayout from '@/layouts/AuthLayout';
-import HomeLayout from '@/layouts/HomeLayout';
-import WhitePageLayout from '@/layouts/WhitePageLayout';
-
-export { AuthLayout, HomeLayout, WhitePageLayout };
