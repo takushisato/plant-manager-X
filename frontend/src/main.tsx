66435f3d448110011642070538f72d12c794bdf8
takushisato <takuchanpower@gmail.com>
2025-04-07 18:15:05 +0900

    不要ファイル削除

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, createSystem } from '@chakra-ui/react';
 import App from './App.tsx';
-import './index.css';
 
 const system = createSystem();
 
