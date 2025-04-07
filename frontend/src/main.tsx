27cfe10558d161b101710716d3a304f7f62b46e2
takushisato <takuchanpower@gmail.com>
2025-04-07 18:32:44 +0900

    環境の更新

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, createSystem } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
-
-const system = createSystem();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider value={system}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
