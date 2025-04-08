2484870e580e473b6f7df0baf160fa1a1426463f
takushisato <takuchanpower@gmail.com>
2025-04-08 14:29:37 +0900

    axiosの例外処理を整備

---
+++ frontend/src/domain/api/error-response.ts
@@ -0,0 +1,3 @@
+export type ErrorResponse = {
+    [key: string]: string[];
+};
---
+++ frontend/src/domain/api/process-error-response.ts
@@ -0,0 +1,24 @@
+import {
+  BadRequestException,
+  ClientException,
+  ConflictException,
+  NotFoundException,
+  ServerException,
+  UnauthorizedException,
+} from "@/domain/common/exceptions/api-exceptions";
+
+export function processErrorResponse(status: number, message: string) {
+  if (status === 401) {
+    throw new UnauthorizedException(status, message);
+  } else if (status === 400) {
+    throw new BadRequestException(status, message);
+  } else if (status === 404) {
+    throw new NotFoundException(status, message);
+  } else if (status === 409) {
+    throw new ConflictException(status, message);
+  } else if (status >= 402 && status < 500) {
+    throw new ClientException(status, message);
+  } else if (status >= 500) {
+    throw new ServerException(status, message);
+  }
+}
