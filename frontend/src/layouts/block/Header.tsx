0f980958ebee40cc08d13ae0d65d661b470aaea7
takushisato <takuchanpower@gmail.com>
2025-04-08 12:15:42 +0900

    ヘッダーを作りこみ

--- frontend/src/layouts/block/Header.tsx
+++ frontend/src/layouts/block/Header.tsx
@@ -3,10 +3,15 @@ import { HamburgerIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  // TODO: ログイン中のユーザー名を取得する
+  const username = "山田太郎";
+
   return (
     <Box as="header" bg="teal.500" px={4} py={3} color="white">
       <Flex align="center">
-        <Heading size="md">MySite</Heading>
+        <Heading size="md" as={Link} to="/">
+          工場管理くん
+        </Heading>
         <Spacer />
         <Menu>
           <MenuButton
@@ -15,12 +20,39 @@ const Header = () => {
             variant="outline"
             color="white"
             borderColor="white"
-            _hover={{ bg: 'teal.600' }}
+            _hover={{ bg: "teal.600" }}
           />
-          <MenuList>
-            <MenuItem as={Link} to="/">Home</MenuItem>
-            <MenuItem as={Link} to="/about">About</MenuItem>
-            <MenuItem as={Link} to="/contact">Contact</MenuItem>
+          <MenuList color="green.700">
+            <Box
+              textAlign="center"
+              px={3}
+              py={2}
+              fontSize="sm"
+              color="green.400"
+            >
+              ログイン中: {username}さん
+            </Box>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              ログイン
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              資材管理
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              受注管理
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              生産計画
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              勤怠管理
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              不具合情報
+            </MenuItem>
+            <MenuItem as={Link} to="/" display="block" textAlign="center">
+              社内メール
+            </MenuItem>
           </MenuList>
         </Menu>
       </Flex>
