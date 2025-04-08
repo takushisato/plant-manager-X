1ec67edf8ccb421053dfab8cfc8317da777e85cf
takushisato <takuchanpower@gmail.com>
2025-04-08 15:20:25 +0900

    URLからメソッドを特定する関数を追加

--- frontend/src/domain/api/apiClient.ts
+++ frontend/src/domain/api/apiClient.ts
@@ -1,8 +1,28 @@
 import axios from "axios";
 import type { AxiosResponse, AxiosError, AxiosRequestConfig } from "axios";
-import { apiBaseUrl } from "@/utils/api-base-url";
 import type { ErrorResponse } from "@/domain/api/error-response";
 import { processErrorResponse } from "@/domain/api/process-error-response";
+import { endpoints } from "@/utils/apiUrls";
+
+/**
+ * URL に応じて HTTP メソッドを判定
+ */
+const detectMethod = (url: string): "GET" | "POST" | "PUT" | "DELETE" => {
+  const methodMap = {
+    get: endpoints.get,
+    post: endpoints.post,
+    put: endpoints.put,
+    delete: endpoints.delete,
+  };
+
+  for (const method of Object.keys(methodMap) as (keyof typeof methodMap)[]) {
+    const entries = Object.values(methodMap[method]);
+    if (entries.includes(url)) {
+      return method.toUpperCase() as "GET" | "POST" | "PUT" | "DELETE";
+    }
+  }
+  throw new Error("Invalid URL");
+};
 
 /**
  * Axiosラッパー関数
@@ -17,10 +37,14 @@ export const apiClient = async <T>(config: AxiosRequestConfig): Promise<T> => {
       ...(token && { Authorization: `Token ${token}` }), // トークンが存在する場合のみ Authorization を追加
     };
 
+    const method = config.method || detectMethod(config.url || "");
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
     const response: AxiosResponse<T> = await axios({
-      baseURL: apiBaseUrl(),
+      baseURL: baseUrl,
       ...config,
       headers,
+      method,
     });
 
     return response.data;
