95338463fc1fb2d131962e754795d211366e123b
takushisato <takuchanpower@gmail.com>
2025-04-14 13:37:32 +0900

    パーミッションの型を追加

--- frontend/src/domain/auth/user.ts
+++ frontend/src/domain/auth/user.ts
@@ -2,6 +2,7 @@ export type User = {
   id: number;
   name: string;
   email: string;
+  permission: Permission;
 };
 
 export type AuthStore = {
@@ -9,4 +10,19 @@ export type AuthStore = {
   restoreSession: () => Promise<void>;
   login: (email: string, password: string) => Promise<{ auth_token: string }>;
   logout: () => Promise<void>;
+};
+
+export type Permission = {
+  staff_hub_access: boolean;
+  material_access: boolean;
+  can_manage_own_attendance: boolean;
+  can_manage_all_attendance: boolean;
+  can_view_production_plan: boolean;
+  can_edit_production_plan: boolean;
+  can_view_order: boolean;
+  can_edit_order: boolean;
+  can_view_defect: boolean;
+  can_edit_defect: boolean;
+  mail_access: boolean;
+  master_data_access: boolean;
 };
