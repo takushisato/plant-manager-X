ec326a69ff05c54c9697b8e68c65972e56eb3947
takushisato <takuchanpower@gmail.com>
2025-04-15 14:37:19 +0900

    資材一覧

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import Material from "@/pages/Material";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/material" element={<Material />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Material.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/layouts/Layout";
+const Material = () => {
+  return (
+    <Layout>
+      <p>資材一覧ページ</p>
+    </Layout>
+  );
+};
+
+export default Material;
