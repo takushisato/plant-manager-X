e792b86cc75d4a16040542b5f22fc497826cf239
takushisato <takuchanpower@gmail.com>
2025-04-22 14:00:32 +0900

    updateページを作りこみ

--- frontend/src/pages/OrderUpdate.tsx
+++ frontend/src/pages/OrderUpdate.tsx
@@ -1,9 +1,19 @@
 import Layout from "@/layouts/Layout";
 import OrderCreateForm from "@/components/order/OrderCreateForm";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const OrderUpdate = () => {
+  const { id } = useParams();
+  const [orderId, setOrderId] = useState<string | undefined>(id);
+
+  useEffect(() => {
+    setOrderId(id);
+  }, [id]);
+
   return (
     <Layout>
-      <OrderCreateForm />
+      <OrderCreateForm id={orderId} />
     </Layout>
   );
 };
