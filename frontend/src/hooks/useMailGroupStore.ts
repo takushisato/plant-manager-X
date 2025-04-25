8f49f38f3a50ce0c74f5b60a23500e901b1e3bd3
takushisato <takuchanpower@gmail.com>
2025-04-25 15:51:42 +0900

    メールグループのリストのモックを移動

--- frontend/src/hooks/useMailGroupStore.ts
+++ frontend/src/hooks/useMailGroupStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { MailGroupList } from "@/types/mail";
 import { User } from "@/types/user";
+import { mockMailGroupList } from "@/fixtures/mail-group-list";
 
 type MailGroupStore = {
   mailGroupList: MailGroupList[];
@@ -17,36 +18,8 @@ export const useMailGroupStore = create<MailGroupStore>((set) => ({
    * メールグループ一覧を取得する
    */
   getMailGroupList: () => {
-    const mockMailGroupList: MailGroupList[] = [
-      {
-        id: 1,
-        group_title: "テストグループ1",
-        note: "テストグループ1の説明",
-        records: [
-          {
-            recipient_user: 1,
-            recipient_user_name: "山田太郎",
-          },
-          {
-            recipient_user: 2,
-            recipient_user_name: "山田花子",
-          },
-        ],
-      },
-      {
-        id: 2,
-        group_title: "テストグループ2",
-        note: "テストグループ2の説明",
-        records: [
-          {
-            recipient_user: 1,
-            recipient_user_name: "山田太郎",
-          },
-        ],
-      },
-    ];
-    set({ mailGroupList: mockMailGroupList });
-    console.log(mockMailGroupList);
+    const mailGroupList = mockMailGroupList;
+    set({ mailGroupList });
   },
 
   /*
