6c4efaa83480f26d73d3365eddeb8a95d57d4bcb
takushisato <takuchanpower@gmail.com>
2025-04-25 17:41:48 +0900

    不要部分を削除

--- frontend/src/tests/pages/MailCreate.test.tsx
+++ frontend/src/tests/pages/MailCreate.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import MailCreate from "@/pages/MailCreate";
