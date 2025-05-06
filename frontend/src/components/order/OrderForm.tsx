2d2d5c0d702ce9c65f67020279613465ac02cae6
taku820801 <takuchanpower@gmail.com>
2025-05-06 17:03:12 +0900

    受注にtipsを追加（叩き台）

--- frontend/src/components/order/OrderForm.tsx
+++ frontend/src/components/order/OrderForm.tsx
@@ -1,7 +1,8 @@
-import { Box, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Button } from "@chakra-ui/react";
 import { useOrderStore } from "@/hooks/useOrderStore";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import InputWithTooltip from "@/components/common/InputWithTooltip";
 
 const OrderForm = () => {
   const {
@@ -80,73 +81,83 @@ const OrderForm = () => {
   };
 
   return (
-    <Box
-      as="form"
-      onSubmit={handleSubmit}
-      w="100%"
-      maxW="1200px"
-      mx="auto"
-      data-testid="order-form"
-    >
+    <Box as="form" onSubmit={handleSubmit} w="100%" maxW="1200px" mx="auto" data-testid="order-form">
       <FormControl>
         <FormLabel>顧客名</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="顧客名"
+          name="customer_name"
+          tooltip="顧客名を入力してください"
           type="text"
           value={customer_name}
           onChange={(e) => setCustomerName(e.target.value)}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>注文番号</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="注文番号"
+          name="order_number"
+          tooltip="注文番号を入力してください"
           type="text"
           value={order_number}
           onChange={(e) => setOrderNumber(e.target.value)}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>注文日</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="注文日"
+          name="order_date"
+          tooltip="注文日を入力してください"
           type="date"
           value={order_date}
           onChange={(e) => setOrderDate(e.target.value)}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>商品名</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="商品名"
+          name="product_name"
+          tooltip="商品名を入力してください"
           type="text"
           value={product_name}
           onChange={(e) => setProductName(e.target.value)}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>数量</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="数量"
+          name="quantity"
+          tooltip="数量を入力してください"
           type="number"
           value={quantity}
           onChange={(e) => setQuantity(parseInt(e.target.value))}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>価格</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="価格"
+          name="price"
+          tooltip="価格を入力してください"
           type="number"
           value={price}
           onChange={(e) => setPrice(parseInt(e.target.value))}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>納期</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="納期"
+          name="deadline"
+          tooltip="納期を入力してください"
           type="date"
           value={deadline}
           onChange={(e) => setDeadline(e.target.value)}
         />
       </FormControl>
       <FormControl>
-        <FormLabel>備考</FormLabel>
-        <Input
+        <InputWithTooltip
+          label="備考"
+          name="note"
+          tooltip="備考を入力してください"
           type="text"
           value={note}
           onChange={(e) => setNote(e.target.value)}
