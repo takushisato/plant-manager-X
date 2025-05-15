ef1c774e778c59db0956e4f50eee7f05f4303788
taku820801 <takuchanpower@gmail.com>
2025-05-15 23:04:19 +0900

    apiエンドポイント追加

--- frontend/src/utils/apiUrls.ts
+++ frontend/src/utils/apiUrls.ts
@@ -9,6 +9,8 @@ export const endpoints = {
   },
   put: {
     users: (id: string) => `/api/auth/users/${id}`,
+    materialReceiveStock: (id: string) => `/api/materials/receive_stock/${id}`,
+    materialUseStock: (id: string) => `/api/materials/use_stock/${id}`,
   },
   delete: {
     users: (id: string) => `/api/auth/users/${id}`,
