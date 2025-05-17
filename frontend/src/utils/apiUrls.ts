753cd774ba7f6c0024cf373bf463188da2a6c56e
taku820801 <takuchanpower@gmail.com>
2025-05-17 16:21:59 +0900

    APIのバグ修正

--- frontend/src/utils/apiUrls.ts
+++ frontend/src/utils/apiUrls.ts
@@ -4,15 +4,15 @@ export const endpoints = {
     logout: "/api/auth/token/logout",
   },
   get: {
-    users: "/api/auth/custom/users/me",
-    materialList: "/api/materials",
+    users: "/api/auth/custom/users/me/",
+    materialList: "/api/materials/",
   },
   put: {
-    users: (id: number) => `/api/auth/users/${id}`,
-    materialReceiveStock: (id: number) => `/api/materials/receive_stock/${id}`,
-    materialUseStock: (id: number) => `/api/materials/use_stock/${id}`,
+    users: (id: number) => `/api/auth/users/${id}/`,
+    materialReceiveStock: (id: number) => `/api/materials/receive_stock/${id}/`,
+    materialUseStock: (id: number) => `/api/materials/use_stock/${id}/`,
   },
   delete: {
-    users: (id: number) => `/api/auth/users/${id}`,
+    users: (id: number) => `/api/auth/users/${id}/`,
   },
 };
