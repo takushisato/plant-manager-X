d4f40cac0c8f7994639047888192dbbc6a1d4161
taku820801 <takuchanpower@gmail.com>
2025-05-21 23:34:08 +0900

    template修正

--- frontend/src/pages/AttendanceList.tsx
+++ frontend/src/pages/AttendanceList.tsx
@@ -1,11 +1,10 @@
 import Layout from "@/layouts/Layout";
 import GenericTable from "@/components/common/GenericTable";
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { Column } from "@/types/common/generic-table";
 import { useAttendanceStore } from "@/hooks/useAttendanceStore";
 import { useEffect } from "react";
 import { UserAttendanceList } from "@/types/attendance";
-
 
 const AttendanceList = () => {
   const columns: Column<UserAttendanceList>[] = [
@@ -13,13 +12,8 @@ const AttendanceList = () => {
     { header: "今月の出勤数", accessor: "attendance_count" },
     { header: "詳細", accessor: "detail" },
   ];
-  const {
-    currentYearMonth,
-    handlePrevMonth,
-    handleNextMonth,
-    allUserAttendanceList,
-    getUserAttendanceList,
-  } = useAttendanceStore();
+  const { currentYearMonth, handlePrevMonth, handleNextMonth, allUserAttendanceList, getUserAttendanceList } =
+    useAttendanceStore();
 
   useEffect(() => {
     getUserAttendanceList();
@@ -28,19 +22,20 @@ const AttendanceList = () => {
   return (
     <Layout>
       <Box mt={4}>
-        <Flex
-          alignItems="center"
-          gap={2}
-          maxW="1200px"
-          mx="auto"
-          fontSize="24px"
-          mb={4}
-        >
-          <Button onClick={handlePrevMonth}>←</Button>
-          <h1>{currentYearMonth}</h1>
-          <Button onClick={handleNextMonth}>→</Button>
-        </Flex>
-        <GenericTable columns={columns} data={allUserAttendanceList} />
+        {allUserAttendanceList.length === 0 ? (
+          <Flex justifyContent="center" alignItems="center" h="100%">
+            <Text>保存された出勤簿がありません</Text>
+          </Flex>
+        ) : (
+          <>
+            <Flex alignItems="center" gap={2} maxW="1200px" mx="auto" fontSize="24px" mb={4}>
+              <Button onClick={handlePrevMonth}>←</Button>
+              <h1>{currentYearMonth}</h1>
+              <Button onClick={handleNextMonth}>→</Button>
+            </Flex>
+            <GenericTable columns={columns} data={allUserAttendanceList} />
+          </>
+        )}
       </Box>
     </Layout>
   );
