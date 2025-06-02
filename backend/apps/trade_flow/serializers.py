e87725cea2f89b1096b36667d3e2b0108cddc1ac
taku820801 <takuchanpower@gmail.com>
2025-06-02 22:50:18 +0900

    シリアライザーの更新

--- backend/apps/trade_flow/serializers.py
+++ backend/apps/trade_flow/serializers.py
@@ -23,9 +23,16 @@ class OrderListSerializer(serializers.ModelSerializer):
 
 class OrderDetailSerializer(serializers.ModelSerializer):
     customer_name = serializers.CharField(source='customer.customer_name', read_only=True)
+    customer = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Order
         fields = [
             'id', 'order_number', 'order_date', 'product_name', 'quantity',
             'price', 'deadline', 'note', 'customer', 'customer_name'
         ]
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
