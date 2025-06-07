9f851e297819381491276e25f57c6ebd6db7ea5d
taku820801 <takuchanpower@gmail.com>
2025-06-07 22:34:29 +0900

    APIテスト追加

--- backend/apps/trade_flow/urls.py
+++ backend/apps/trade_flow/urls.py
@@ -5,5 +5,5 @@ from .views import OrderView, OrderDetailView, CustomerView
 urlpatterns = [
     path("orders/", OrderView.as_view(), name="order"),
     path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
-    path("customers/", CustomerView.as_view(), name="customer"),
+    path("customers/", CustomerView.as_view(), name="customer-list"),
 ]
