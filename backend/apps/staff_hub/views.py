8e6e8f7b4d1ad11d6c0086a9da79ed109391fa85
taku820801 <takuchanpower@gmail.com>
2025-06-14 22:47:14 +0900

    全ユーザーの名前とIDを取得するAPIを作成

--- backend/apps/staff_hub/views.py
+++ backend/apps/staff_hub/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models.permission import Permission
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, AllUsersSerializer
+from .models.user import User
+
 
 class CustomUserMeView(APIView):
     permission_classes = [IsAuthenticated]
@@ -10,4 +12,13 @@ class CustomUserMeView(APIView):
     def get(self, request):
         user = request.user
         serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+
+
+class AllUsersView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = AllUsersSerializer(users, many=True)
         return Response(serializer.data)
